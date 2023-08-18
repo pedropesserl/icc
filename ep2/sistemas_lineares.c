deba8bc08625de1b09626cfa3df01b8f54d1d5ea
Pedro F. Pesserl <fpesserl7@gmail.com>
2023-08-18 17:42:36 -0300

    ler entradas

--- ep2/sistemas_lineares.c
+++ ep2/sistemas_lineares.c
@@ -7,36 +7,25 @@ double **cria_matriz(size_t tam, double *data) {
         fprintf(stderr, "Erro de alocação de memória (cria_matriz())\n");
         exit(1);
     }
-    for (int k = 0; k < tam; k++)
+    for (size_t k = 0; k < tam; k++)
         m[k] = &(data[k*tam]);
     return m;
 }
 
 int main() {
     size_t tam;
+    scanf("%ld", &tam);
+
     double *entradas = (double*)calloc(tam*tam, sizeof(double));
     double *B = (double*)calloc(tam, sizeof(double));
     double *X = (double*)calloc(tam, sizeof(double));
-
-    scanf("%ld", &tam);
-    for (int i = 0; i < tam; i++) {
-        for (int j = 0; j < tam; j++)
+    for (size_t i = 0; i < tam; i++) {
+        for (size_t j = 0; j < tam; j++)
             scanf("%lf", &(entradas[i*tam+j]));
         scanf("%lf", &(B[i]));
     }
 
     double **A = cria_matriz(tam, entradas);
-    printf("A:\n");
-    for (int i = 0; i < tam; i++) {
-        for (int j = 0; j < tam; j++)
-            printf("%lf ", A[i][j]);
-        printf("\n");
-    }
-
-    printf("B:\n");
-    for (int i = 0; i < tam; i++)
-        printf("%lf ", A[i][j]);
-    printf("\n");
 
     free(entradas);
     free(A);
