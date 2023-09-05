ce30503ffd96f1dd93519344be131de7f5c20b48
gabriellisboaconegero <gabriellisboaconegero@gmail.com>
2023-09-05 00:42:47 -0300

    Adicionando find_max, troca_linha e teste.sh
    find_max e troca_linha sao usados nos metodos
    com pivotamento. Alterei o imprime solução para receber
    um ponteiro para struct Sistema, pois fica padronizado sempre
    passar ponteiros.
    Adicionei um script de teste, ler teste.sh para saber como funciona.

--- ep2/sistemas_lineares.c
+++ ep2/sistemas_lineares.c
@@ -89,16 +89,74 @@ void sem_pivoteamento(struct Sistema *s) {
     exit(1);
 }
 
-void imprime_solucao(struct Sistema s) {
-    if (s.solucao_unica != 1) {
+// retorna linha com maior valor em módulo na coluna linha_pivo na qual i <= linha_pivo
+size_t find_max(struct Sistema *s, size_t linha_pivo) {
+    double max = 0;
+    size_t id_max = 0;
+    if (!s || linha_pivo > s->ordem)
+        return 0;
+       
+    if (linha_pivo == s->ordem - 1) // Se tiver apenas um elemento nas
+        return linha_pivo;          //linhas de baixos de linha_pivo
+
+    for (size_t i = linha_pivo; i < s->ordem; i++) {
+        if (fabs(s->A[i][linha_pivo]) > max) {
+            max = fabs(s->A[i][linha_pivo]);
+            id_max = i;
+        }
+    }
+
+    return id_max;
+}
+
+// retorna 1 se tudo ocorrer corretamente
+int troca_linha(struct Sistema *s, size_t linha1, size_t linha2) {
+    double *tmp;
+
+    if (!s || linha1 > s->ordem || linha2 > s->ordem)
+        return 0;
+    
+    if (linha1 == linha2) // nao trocar se for a memsa linha
+        return 1;
+
+    tmp = s->A[linha1];
+    s->A[linha1] = s->A[linha2];
+    s->A[linha2] = tmp;
+
+    double tmp2;
+    tmp2 = s->B[linha1];
+    s->B[linha1] = s->B[linha2];
+    s->B[linha2] = tmp2;
+
+    return 1;
+}
+
+void imprime_solucao(struct Sistema *s) {
+    if (s->solucao_unica != 1) {
         printf("O sistema não possui solução única.\n");
         return;
     }
     printf("X = [ ");
-    for (size_t i = 0; i < s.ordem; i++) {
-        printf("%.5lf ", s.X[i]);
+    for (size_t i = 0; i < s->ordem; i++) {
+        printf("%.5lf ", s->X[i]);
     }
     printf("]\n");
+}
+
+void imprime_sistema(struct Sistema *s) {
+    printf("    ┏\n");
+    for (size_t i = 0; i < s->ordem; i++) {
+        if (i == s->ordem/2)
+            printf("A = ┃ ");
+        else
+            printf("    ┃ ");
+        
+        for (size_t j = 0; j < s->ordem; j++) {
+            printf("%.5lf ", s->A[i][j]);
+        }
+        printf("   %.5lf\n", s->B[i]);
+    }
+    printf("    ┗\n");
 }
 
 int main() {
@@ -108,7 +166,7 @@ int main() {
     struct Sistema s = cria_sistema(ordem);
 
     retrosub(&s);
-    imprime_solucao(s);
+    imprime_solucao(&s);
 
     destroi_sistema(&s);
     return 0;
---
+++ ep2/teste.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/env bash
+
+if [[ ! -x $1  ]]; then
+    echo "$1 não é executavél"
+fi
+acc=()
+prog=$1
+function execute_test {
+    if [[ -n $acc ]]; then
+        echo $acc | $prog
+    fi
+}
+
+# ISSO é bizarro, mas eu agrupei o while pq como o pipe cria um SubShell
+# Então a variavel acc n vai ser atualizada depois do while, então vai estar vaiza
+# dessa forma se chamar execute_test nao vai acontecer nada.
+# Da para criar uma funcao e passar o pipe para ela tambem.
+# function parse {
+#   <codigo que usa acc>
+# }
+#
+# grep -v "^#" teste.dat | parse
+#
+# doido!!!!!
+#
+# Vi como resolver aqui:
+# https://serverfault.com/questions/259339/bash-variable-loses-value-at-end-of-while-read-loop
+grep -v "^#" teste.dat | (while read line; do
+    if [[ $line == "--break--" ]]; then
+        execute_test
+        exit 0
+    elif [[ -n $line ]]; then
+        acc+="$line "
+    else
+        execute_test
+        acc=()
+    fi
+done
+execute_test)
