8b24e4464cfb86a0eb62ae34e2e820f30f4522f7
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-09-23 18:53:31 -0300

    [ep3] implementar interpolação de newton
    a alocação da tabela de diferenças divididas é feita FORA da função de interpolação. podemos ver se vamos mudar isso ou manter desse jeito

--- ep3/interpola.c
+++ ep3/interpola.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "utils.h"
+#include <likwid.h>
 
 #define MEM_ERR do { \
         fprintf(stderr, "Erro de alocação de memória: %s:%d (%s)\n", __FILE__, __LINE__, __func__);             \
@@ -11,9 +11,20 @@ struct Ponto {
     double x, fx;
 };
 
-// presume que os valores de x inseridos estão em ordem crescente
+void cria_matriz(size_t tam, double **m, double *data) {
+    for (size_t k = 0; k < tam; k++)
+        m[k] = &(data[k*tam]);
+}
+
 int esta_no_intervalo(double x, struct Ponto *tabela, size_t n) {
-    return tabela[0].x <= x && x <= tabela[n-1].x;
+    double menor = tabela[0].x, maior = tabela[0].x;
+    for (size_t i = 1; i < n; i++) {
+        if (tabela[i].x > maior)
+            maior = tabela[i].x;
+        if (tabela[i].x < menor)
+            menor = tabela[i].x;
+    }
+    return menor <= x && x <= maior;
 }
 
 // como é garantido que a entrada do programa nunca será um dos xi da tabela,
@@ -39,17 +50,32 @@ double interpolacao_lagrange(double x, struct Ponto *tabela, size_t n) {
     return px;
 }
 
-double interpolacao_newton(double x, struct Ponto *tabela, size_t n) {
-    double px = 0.0;
-    
 
+double interpolacao_newton(double x, double **dif_div, struct Ponto *tabela, size_t n) {
+    // preenche a matriz de diferenças divididas
+    // a linha i contém as n-i diferenças divididas de ordem i
+    // a diferença dividida de ordem 0 de xj (f[xj]) é igual a f(xj)
+    for (size_t j = 0; j < n; j++)
+        dif_div[0][j] = tabela[j].fx;
+
+    for (size_t i = 1; i < n; i++)
+        for (size_t j = 0; j < n-i; j++)
+            dif_div[i][j] = (dif_div[i-1][j+1] - dif_div[i-1][j]) / (tabela[i+j].x - tabela[j].x);
+    
+    // calcula o valor de px usando a matriz
+    double px = 0.0;
+    double m = 1.0;
+    for (size_t i = 0; i < n; i++) {
+        px += dif_div[i][0] * m;
+        m *= x - tabela[i].x;
+    }
 
     return px;
 }
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "Use exatamente um argumento (valor double).\n");
+        fprintf(stderr, "Uso: %s <valor_a_ser_interpolado>\n", argv[0]);
         return 1;
     }
 
@@ -59,8 +85,7 @@ int main(int argc, char **argv) {
     scanf("%ld", &n);
 
     struct Ponto *tabela = (struct Ponto*)calloc(n, sizeof(struct Ponto));
-    if (!tabela)
-        MEM_ERR;
+    if (!tabela) MEM_ERR;
     for (size_t i = 0; i < n; i++)
         scanf("%lf %lf", &(tabela[i].x), &(tabela[i].fx));
 
@@ -74,14 +99,23 @@ int main(int argc, char **argv) {
     real_t t_l = timestamp() - t;
     printf("%1.8e\n", pxe_l);
 
+    // cria matriz para armazenar diferenças divididas no método de newton
+    double **dif_div = (double**)calloc(n, sizeof(double*));
+    if (!dif_div) MEM_ERR;
+    double *data = (double*)calloc(n*n, sizeof(double));
+    if (!data) MEM_ERR;
+    cria_matriz(n, dif_div, data);
+
     t = timestamp();
-    double pxe_n = interpolacao_newton(xe, tabela, n);
+    double pxe_n = interpolacao_newton(xe, dif_div, tabela, n);
     real_t t_n = timestamp() - t;
     printf("%1.8e\n", pxe_n);
 
-    printf("%lf\n", t_l);
-    printf("%lf\n", t_n);
+    printf("%1.8e\n", t_l);
+    printf("%1.8e\n", t_n);
 
+    free(data);
+    free(dif_div);
     free(tabela);
 
     return 0;
