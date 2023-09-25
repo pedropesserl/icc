60c6612149290e4646a0e138900f7e2533362dde
gabriellisboaconegero <gabriellisboaconegero@gmail.com>
2023-09-24 22:54:47 -0300

    [ep3] Modificando saida do mflops.sh

--- ep3/mflops.sh
+++ ep3/mflops.sh
@@ -6,40 +6,35 @@ CPU=3
 xe=$1
 entrada=$2
 if [[ -z $xe || ! -f $entrada ]]; then
-    echo "Usage: ./mflops.sh <xe> <arquivo_entrada> [arquivo_saida]"
-    echo "  Comportamento default:"
-    echo "      Arquivo arquivo_saida=teste.out se não for passado arquivo_saida"
+    echo "Usage: ./mflops.sh <xe> <arquivo_entrada>"
     exit 0
 fi
-saida=${3:-"teste.out"}
 
 echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
 
-echo "Compilando programa (sem otimização)."
-make purge
-make sem_otimizacao
+make purge > /dev/null
+make sem_otimizacao > /dev/null
 
-echo "-------------- (sem otimização) --------------" > $saida
-echo "Saida da execução:" >> $saida
+echo "-------------- (sem otimização) --------------"
+echo "Saida da execução:"
 likwid-perfctr -C ${CPU} -g $METRICA -m ./interpola $xe < $entrada > tmp
-awk 'BEGIN {RS="--------------------------------------------------------------------------------\n"} NR==3{print}' tmp >> $saida
-echo "Análise LIKWID:" >> $saida
+awk 'BEGIN {RS="--------------------------------------------------------------------------------\n"} NR==3{print}' tmp
+echo "Análise LIKWID:"
 grep -e '| *DP MFLOP' -e '^Region' tmp | \
-    awk 'BEGIN{a=""} NR%2==1{sub(",", "", $2); a=a""$2} NR%2==0{print a": "$5" MFLOP/s"; a=""}' >> $saida
-echo "-------------- (sem otimização) --------------" >> $saida
+    awk 'BEGIN{a=""} NR%2==1{sub(",", "", $2); a=a""$2} NR%2==0{print a": "$5" MFLOP/s"; a=""}'
+echo "-------------- (sem otimização) --------------"
 
-echo "Compilando programa (com otimização)."
-make purge
-make
+make purge > /dev/null
+make > /dev/null
 
-echo "-------------- (com otimização) --------------" >> $saida
-echo "Saida da execução:" >> $saida
+echo "-------------- (com otimização) --------------"
+echo "Saida da execução:"
 likwid-perfctr -C ${CPU} -g $METRICA -m ./interpola $xe < $entrada > tmp
-awk 'BEGIN {RS="--------------------------------------------------------------------------------\n"} NR==3{print}' tmp >> $saida
-echo "Análise LIKWID:" >> $saida
+awk 'BEGIN {RS="--------------------------------------------------------------------------------\n"} NR==3{print}' tmp
+echo "Análise LIKWID:"
 grep -e '| *DP MFLOP' -e '^Region' tmp | \
-    awk 'BEGIN{a=""} NR%2==1{sub(",", "", $2); a=a""$2} NR%2==0{print a": "$5" MFLOP/s"; a=""}' >> $saida
-echo "-------------- (com otimização) --------------" >> $saida
+    awk 'BEGIN{a=""} NR%2==1{sub(",", "", $2); a=a""$2} NR%2==0{print a": "$5" MFLOP/s"; a=""}'
+echo "-------------- (com otimização) --------------"
 
 rm -rf tmp
 
