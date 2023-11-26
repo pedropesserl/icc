8df6ad497ddb766a783dd9089f1013cd528cb503
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-11-26 17:28:19 -0300

    [t2] corrigir script de gerar logs
    tava usando o programa v1 pra gerar os dois logs burrao

--- t2/logs_likwid.sh
+++ t2/logs_likwid.sh
@@ -9,7 +9,6 @@ fi
 
 GRUPOS="FLOPS_DP L3 L2CACHE"
 N="64 128 200 256 512 600 800 1024 2000 3000 4096 6000 7000 10000 50000 100000 1000000 10000000 100000000"
-VERSOES="v1 v2"
 
 make purge > /dev/null
 gcc -o gera_entrada gera_entrada.c
@@ -18,13 +17,17 @@ make > /dev/null
 echo "Gerando logs do Likwid..."
 for grupo in $GRUPOS; do
     for n in $N; do
-        for versao in $VERSOES; do
-            echo "Gerando log $grupo"_"$n"_$versao""
-            echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
-            ./gera_entrada $n | likwid-perfctr -O -C 3 -g $grupo -m v1/ajustePol \
-                > $LOGS/$grupo"_"$n"_$versao.log"
-            echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
-        done
+        echo "Gerando log ${grupo}_${n}_v1"
+        echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+        ./gera_entrada $n | likwid-perfctr -O -C 3 -g $grupo -m v1/ajustePol \
+            > $LOGS/${grupo}_${n}_v1.log
+        echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+
+        echo "Gerando log ${grupo}_${n}_v2"
+        echo "performance" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
+        ./gera_entrada $n | likwid-perfctr -O -C 3 -g $grupo -m ajustePol \
+            > $LOGS/${grupo}_${n}_v2.log
+        echo "powersave" > /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
     done
     echo "Logs de $grupo gerados no diretório $LOGS."
 done
