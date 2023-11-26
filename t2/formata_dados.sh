5bcc67a78fcf16302b002734465d4bab22b0dda7
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-11-25 23:55:11 -0300

    [t2] formatar dados do teste de tempo

---
+++ t2/formata_dados.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+DADOS="./resultados/dados"
+if [ ! -d $DADOS ]; then
+    mkdir $DADOS
+fi
+
+N="64"
+FUNCOES="gera_sl resolve_sl calcula_residuos"
+CAMPOS=("^DP" "L2 miss ratio" "L3 bandwidth")
+VERSOES="v1 v2"
+
+echo 'Formatando dados do teste "tempo"...'
+rm -f $DADOS/tempo*
+for n in $N; do
+    # Nos arquivos .log, o tempo da geração do SL está na linha 7, da solução
+    # na linha 8 e do cálculo dos resíduos na linha 9
+    j=7
+    for versao in $VERSOES; do
+        for funcao in $FUNCOES; do
+            echo -n "$n " >> $DADOS/tempo_$funcao"_$versao.dat"
+            # Escolhi pegar a saída contida nos arquivos L3 arbitrariamente
+            sed "${j}q;d" $LOGS/L3_$n"_"$versao.log >> $DADOS/tempo_$funcao"_$versao.dat"
+            ((j++))
+        done
+    done
+done
+echo "Pronto." 
+
